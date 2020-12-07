88714cf1b2e70423d2399c6255924395fa00e2ed
Khaled-IamZ <74547726+Khaled-IamZ@users.noreply.github.com>
2020-12-07 21:56:53 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,7 +3,7 @@
 #Centos 7
 #Created May 25, 2018
 #Latest Softether Server Version for May 2018 v4.27-9667-beta-2018.05.26-tree
-#Coded by Jayson Velagio
+#Coded by Khaled A. Moneim
 #Open Source Project From https://www.softether.org/
 #From university of Tsukuba, Japan
 #--------------------------------------------------
@@ -31,7 +31,7 @@ chmod 600 *
 chmod 700 vpncmd
 chmod 700 vpnserver
 #Installing server command
-wget https://raw.githubusercontent.com/Khaled-IamZ/VPN/main/vpn-server.sh --no-check-certificate
+wget https://raw.githubusercontent.com/Khaled-IamZ/kh/main/vpn-server.sh --no-check-certificate
 mv vpn-server.sh /etc/init.d/vpnserver
 cd /etc/init.d/
 chmod 755 vpnserver
@@ -40,7 +40,7 @@ chkconfig --add vpnserver
 cd /usr/local/vpnserver
 echo ---------------------------------------------
 echo  -e "\033[32;5mVPN SERVER INSTALLED SUCCESSFULLY!\033[0m"
-echo "SoftEther auto installer by JAYSON VELAGIO"
+echo "SoftEther auto installer by Khaled A. Moneim"
 echo "vpncmd is at /usr/local/vpnserver"
 echo ---------------Commands----------------------
 echo /etc/init.d/vpnserver start - to start
