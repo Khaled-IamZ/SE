12f395c500beb0401de0aad4bef41c64b1867a28
Khaled-IamZ <74547726+Khaled-IamZ@users.noreply.github.com>
2022-09-06 13:37:22 +0200

    Update install.sh

--- Softether-Client/install.sh
+++ Softether-Client/install.sh
@@ -15,7 +15,7 @@ cd /root
 #yum update -y
 #yum groupinstall "development tools" -y
 #yum install wget -y
-wget http://www.softether-download.com/files/softether/v4.38-9760-rtm-2021.08.17-tree/Linux/SoftEther_VPN_Client/64bit_-_Intel_x64_or_AMD64/softether-vpnclient-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
+wget https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.38-9760-rtm/softether-vpnclient-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
 tar zxf softether-vpnclient-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
 cd vpnclient
 make
