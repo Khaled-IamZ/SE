39c996174e6579f4702803d59e122390c3f67b96
Khaled-IamZ <74547726+Khaled-IamZ@users.noreply.github.com>
2022-09-07 04:09:20 +0200

    Update vpn-client.sh

--- Softether-Client/vpn-client.sh
+++ Softether-Client/vpn-client.sh
@@ -1,16 +1,16 @@
-#!binsh
-# chkconfig 2345 99 01
-# description SoftEther VPN Server
-DAEMON=usrlocalvpnclientvpnclient
-LOCK=varlocksubsysvpnclient
+#!/bin/sh
+# chkconfig: 2345 99 01
+# description: SoftEther VPN Client
+DAEMON=/usr/local/vpnclient/vpnclient
+LOCK=/var/lock/subsys/vpnclient
 TAP_ADDR=192.168.30.150
-test -x $DAEMON  exit 0
-case $1 in
+test -x $DAEMON || exit 0
+case "$1" in
 start)
 $DAEMON start
 touch $LOCK
 sleep 1
-sbinifconfig vpn_myadapter $TAP_ADDR
+sbin/ifconfig vpn_myadapter $TAP_ADDR
 ;;
 stop)
 $DAEMON stop
@@ -21,10 +21,10 @@ $DAEMON stop
 sleep 3
 $DAEMON start
 sleep 1
-sbinifconfig vpn_myadapter $TAP_ADDR
+sbin/ifconfig vpn_myadapter $TAP_ADDR
 ;;
-)
-echo Usage $0 {startstoprestart}
+*)
+echo "Usage: $0 {start|stop|restart}"
 exit 1
 esac
 exit 0
