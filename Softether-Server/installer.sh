ba493cc7075f1973d0e3ca65153cb66769de263a
Khaled-IamZ <74547726+Khaled-IamZ@users.noreply.github.com>
2024-03-18 06:45:41 +0200

    Update installer.sh

--- Softether-Server/installer.sh
+++ Softether-Server/installer.sh
@@ -16,8 +16,8 @@ cd /root
 yum groupinstall "development tools" -y
 #Downloading server files
 yum install wget -y
-wget http://www.softether-download.com/files/softether/v4.38-9760-rtm-2021.08.17-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
-tar zxf softether-vpnserver-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
+wget http://www.softether-download.com/files/softether/v4.42-9798-rtm-2023.06.30-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.42-9798-rtm-2023.06.30-linux-x64-64bit.tar.gz
+tar zxf softether-vpnserver-v4.42-9798-rtm-2023.06.30-linux-x64-64bit.tar.gz
 cd vpnserver
 clear
 echo  -e "\033[31;7mNOTE: ANSWER 1 AND ENTER THREE TIMES FOR THE COMPILATION TO PROCEED\033[0m"
