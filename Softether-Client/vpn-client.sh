ea812ca62e782c5582e99c3248ab62218e0f63c9
Khaled-IamZ <74547726+Khaled-IamZ@users.noreply.github.com>
2024-08-30 04:08:22 +0300

    Update vpn-client.sh

--- Softether-Client/vpn-client.sh
+++ Softether-Client/vpn-client.sh
@@ -3,9 +3,7 @@
 # description: SoftEther VPN Client
 DAEMON=/usr/local/vpnclient/vpnclient
 LOCK=/var/lock/subsys/vpnclient
-TAP_ADDR1=192.168.100.150
-TAP_ADDR2=192.168.110.150
-TAP_ADDR2=192.168.120.150
+TAP_ADDR1=10.10.100.100
 test -x $DAEMON || exit 0
 case "$1" in
 start)
@@ -13,8 +11,6 @@ $DAEMON start
 touch $LOCK
 sleep 1
 sbin/ifconfig vpn_tamiuzz1 $TAP_ADDR1
-sbin/ifconfig vpn_tamiuzz1 $TAP_ADDR2
-sbin/ifconfig vpn_tamiuzz1 $TAP_ADDR3
 ;;
 stop)
 $DAEMON stop
@@ -26,8 +22,6 @@ sleep 3
 $DAEMON start
 sleep 1
 sbin/ifconfig vpn_tamiuzz1 $TAP_ADDR1
-sbin/ifconfig vpn_tamiuzz2 $TAP_ADDR2
-sbin/ifconfig vpn_tamiuzz3 $TAP_ADDR3
 ;;
 *)
 echo "Usage: $0 {start|stop|restart}"
